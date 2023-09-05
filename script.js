5f9cd99b56949b54a002c9765d1c552329c73730
Jakub Kulmaczewski <kuba-aa276@wp.pl>
2023-09-06 00:03:44 +0200

    Add removing functions

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function Display(text) {
   document.getElementById("display").value += text;
 }
+
+function Remove(text) {
+  document.getElementById("display").value = "";
+}
+
+function RemoveLastSign() {
+  const del = document.getElementById("display");
+  const currentValue = display.value;
+  if (currentValue.length > 0) {
+    const newValue = currentValue.slice(0, -1);
+    del.value = newValue;
+  }
+}
